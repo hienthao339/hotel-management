2598fb22b558c53d83277ed9c81b0f4fd95b4c47
hienthao339 <hienthao338@gmail.com>
2022-12-21 19:24:59 +0700

    thong báo

---
+++ Hotel/Models/ViewModels/ThongBaoView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hotel.Models.ViewModels
+{
+    public class ThongBaoView
+    {
+        public int ID_DP { get; set; }
+
+        public bool yeucaudat { get; set; }
+        public bool datphong { get; set; }
+        public bool yeucauhuy { get; set; }
+        public bool huyphong { get; set; }
+        public string message { get; set; }
+        public int state { get; set; }
+    }
+}
